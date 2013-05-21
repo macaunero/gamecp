235be9afe74072fc51e0573521b1f39a7f7cd05a
macaunero <macaunero@gmail.com>
2013-05-21 15:50:50 +0800

    Revert "vars2"
    This reverts commit 85463e065248616b1a61488e53dc9bf524d2b9d3.

--- application/config/vars.php
+++ application/config/vars.php
@@ -8,8 +8,3 @@ $config['website_setting']['title_name'] = 'XXX功能网站系统';
 $config['website_setting']['web_description'] = '';
 $config['website_setting']['web_keywords'] = '';
 $config['website_setting']['footer_name'] = '';
-
-
-
-
-?>
